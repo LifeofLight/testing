06feaf1b6ca2213a9029388759af24e41b1d056d
LifeofLight <boss135792468@gmail.com>
2021-06-09 16:19:29 +0700

    update bmi

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from fastapi import FastAPI
 from fastapi.param_functions import Depends
 
@@ -10,17 +9,4 @@ def bmi(h:int=170 , w:int=50):
     bmi =w/h
 
 
-=======
-from fastapi import FastAPI
-from fastapi.param_functions import Depends
-
-app =FastAPI()
-@app.get('/bmi')
-def bmi(h:int=170 , w:int=50):
-
-    h=(h/100)**2
-    bmi =w/h
-
-
->>>>>>> 56478962320c8bdbf4a185e8df027ec973f9f6d1
     return{'bmi':bmi}
