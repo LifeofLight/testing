7075bd22b3fbdb35b2bfe1abc37c7301503ecf95
LifeofLight <boss135792468@gmail.com>
2021-06-10 17:04:05 +0700

    Update tax.py

--- tax.py
+++ tax.py
@@ -3,7 +3,7 @@ from fastapi.param_functions import Depends
 
 app =FastAPI()
 @app.get('/tax')
-def tax(income:int=50000, disabilities:int=0,coupple:int=0,parents:int=1,chile:int=1,agepF:int=60,agepM:int=55,income_pF:int=200,income_pM:int=200,income_c:int=0):
+def tax(income:int=50000, disabilities:int=0,coupple:str="NH",parents:str="H",chile:int=1,agepF:int=60,agepM:int=55,income_pF:int=200,income_pM:int=200,income_c:int=0):
     income_pF=income_pF*12
     income_pM=income_pM*12
     income_p=[income_pF,income_pM]
@@ -22,13 +22,13 @@ def decresstex(money_Per_Year,disabilities,parents,agep,income_p,coupple,income_
     j=0
     if(disabilities==1):
         money_Per_Year=money_Per_Year-60000
-    if coupple==1&income_c==0:
+    if coupple=="H"&income_c==0:
         money_Per_Year=money_Per_Year-60000
     if chile<2:
         money_Per_Year=money_Per_Year-30000
     elif chile>=2:
         money_Per_Year=money_Per_Year-(60000*chile)
-    if parents==1:
+    if parents=="H":
         for i in agep:
             if i>=60:
                 if income_p[j]<=30000:
